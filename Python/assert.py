bcfa4c15fc380a0c6a5d2e4276e51168c6fed49c
unknown <wellingtonvitorps@gmail.com>
2019-10-23 11:44:37 -0300

    add colisor.html

--- Python/assert.py
+++
@@ -1,9 +0,0 @@
-# Program to demostrate assert statement
-
-x = int(input("Enter a number greater than 0:"))
-
-assert x>=0,"Enter a positive number"
-
-print("The number is positive")
-
-# The program will throw an AssertionError if the user enters a value less than 0
