8bf5e443424d43791ce2667510a44e5be1584d08
bhushanmali96 <35413582+bhushanmali96@users.noreply.github.com>
2020-10-02 11:04:18 +0530

    Employee Management API with SpringBoot

---
+++ Java/EmployeeManagementSystem/src/test/java/com/employeemanager/EmployeeManagementSystem/EmployeeManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.employeemanager.EmployeeManagementSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
