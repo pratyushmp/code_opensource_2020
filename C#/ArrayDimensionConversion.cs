d85061c4cb50bea7bfbd7df5852e1b5b5cd9a21a
AsclepiiusUnknown <55474351+AsclepiiusUnknown@users.noreply.github.com>
2020-10-02 19:55:26 +1000

    Create ArrayDimensionConversion.cs
    Added a script that allows the conversion of a 2 Dimensional array into a 1 dimensional array

---
+++ C#/ArrayDimensionConversion.cs
@@ -0,0 +1,77 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ArrayDimensionConversion
+{
+  class TwoDMatrix
+  {
+    int m, n;
+    int[, ] a;
+    int[] b;
+
+    TwoDMatrix (int x, int y)
+    {
+      m = x;
+      n = y;
+      a = new int[m, n];
+      b = new int[m * n];
+    }
+
+    public void ReadMatrix ()
+    {
+      for (int i = 0; i < m; i++)
+      {
+        for (int j = 0; j < n; j++)
+        {
+          Console.WriteLine ("a[{0},{1}]=", i, j);
+          a[i, j] = Convert.ToInt32 (Console.ReadLine ());
+        }
+      }
+    }
+    public void PrintDimension ()
+    {
+      for (int i = 0; i < m; i++)
+      {
+        for (int j = 0; j < n; j++)
+        {
+          Console.Write ("{0}\t", a[i, j]);
+
+        }
+        Console.Write ("\n");
+      }
+    }
+    public void Convert ()
+    {
+      int k = 0;
+      for (int i = 0; i < m; i++)
+      {
+        for (int j = 0; j < n; j++)
+        {
+          b[k++] = a[i, j];
+        }
+      }
+    }
+    public void PrintOneD ()
+    {
+      for (int i = 0; i < m * n; i++)
+      {
+        Console.WriteLine ("{0}\t", b[i]);
+      }
+    }
+
+    public static void Main (string[] args)
+    {
+      twodmatrix obj = new twodmatrix (2, 3);
+      Console.WriteLine ("Enter the Elements : ");
+      obj.ReadMatrix ();
+      Console.WriteLine ("\t\t Given 2-D Array(Matrix) is : ");
+      obj.PrintDimension ();
+      obj.Convert ();
+      Console.WriteLine ("\t\t Converted 1-D Array is : ");
+      obj.PrintOneD ();
+      Console.ReadLine ();
+    }
+  }
+}
