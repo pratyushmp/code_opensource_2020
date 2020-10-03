db89ffc84b83c4dbc2bfb074a85f23f967c1bd2c
Mayur Panchal <mayurpanchal002@gmail.com>
2020-10-03 22:41:12 +0530

    Added Deno language snippet

---
+++ Deno/hello.js
@@ -0,0 +1,3 @@
+// To execute deno program `deno run filepath/filename.ts` (without quotes)
+
+console.log('Welcome to Deno 🦕 written in JavaScript');
---
+++ Deno/hello.ts
@@ -0,0 +1,4 @@
+// To execute deno program `deno run filepath/filename.ts` (without quotes)
+
+// Copyright 2018-2020 the Deno authors. All rights reserved. MIT license.
+console.log('Welcome to Deno 🦕 written in TypeScript');
