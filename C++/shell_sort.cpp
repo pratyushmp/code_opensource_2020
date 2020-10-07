5ffe36a5b4bea75af918e0c20b6fff7ca398c776
Dakshendra Singh <61901342+daksh80@users.noreply.github.com>
2020-10-07 14:13:57 +0530

    add code in c++
    binary search tree
    avl tree
    bucket sort
    counting sort
    heap sort
    linear search
    merge sort
    radix sort
    shell sort(improved)

--- C++/shell_sort.cpp
+++ C++/shell_sort.cpp
@@ -1,33 +1,54 @@
-#include<iostream>
-using namespace std;
- int main()
- {
-     int n;
-     cin>>n; //array size
-     int a[n];
-     //Input in  array
-     for(int i=0;i<n;i++)
-     {
-         cin>>a[i]; 
-     }
-     //shell sort
-     for(int gap=n;gap>0;gap/=2)
-     {
-         for(int i=gap;i<n;i++)
-         {
-             int j,t=a[i];
-             for( j=i;j>=gap&&a[j-gap]>t;j-=gap)
-             {
-                 a[j]=a[j-gap];
-             }
-             a[j]=t;
-         }
-     }
-     //print orted array
-     for(int i=0;i<n;i++)
-     {
-         cout<<a[i]<<" ";
-     }
-     return 0;
- }
- 
+// C++ implementation of Shell Sort 
+#include <iostream> 
+using namespace std; 
+
+/* function to sort arr using shellSort */
+int shellSort(int arr[], int n) 
+{ 
+	// Start with a big gap, then reduce the gap 
+	for (int gap = n/2; gap > 0; gap /= 2) 
+	{ 
+		// Do a gapped insertion sort for this gap size. 
+		// The first gap elements a[0..gap-1] are already in gapped order 
+		// keep adding one more element until the entire array is 
+		// gap sorted 
+		for (int i = gap; i < n; i += 1) 
+		{ 
+			// add a[i] to the elements that have been gap sorted 
+			// save a[i] in temp and make a hole at position i 
+			int temp = arr[i]; 
+
+			// shift earlier gap-sorted elements up until the correct 
+			// location for a[i] is found 
+			int j;			 
+			for (j = i; j >= gap && arr[j - gap] > temp; j -= gap) 
+				arr[j] = arr[j - gap]; 
+			
+			// put temp (the original a[i]) in its correct location 
+			arr[j] = temp; 
+		} 
+	} 
+	return 0; 
+} 
+
+void printArray(int arr[], int n) 
+{ 
+	for (int i=0; i<n; i++) 
+		cout << arr[i] << " "; 
+} 
+
+int main() 
+{ 
+	int arr[] = {12, 34, 54, 2, 3}, i; 
+	int n = sizeof(arr)/sizeof(arr[0]); 
+
+	cout << "Array before sorting: \n"; 
+	printArray(arr, n); 
+
+	shellSort(arr, n); 
+
+	cout << "\nArray after sorting: \n"; 
+	printArray(arr, n); 
+
+	return 0; 
+} 
